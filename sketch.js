acb10445e35ccb204121c5c4ba2f3dfc2b4968a0
Lars S. Laichter <lars.laichter@gmail.com>
2019-04-11 21:34:35 -0700

    Branching added

--- sketch.js
+++ sketch.js
@@ -30,14 +30,43 @@ function keyPressed() {
   	
 }
 
+function mouseClicked() {
+	console.log(slideNum + " " + mouseX + " " + mouseY);
+	if(slideNum == 1){
+		if((mouseX > 190) && (mouseY > 150) && (mouseX < 590) && (mouseY < 230)){
+			slideNum = 3;
+			console.log(slideNum);
+		}
+		if((mouseX > 190) && (mouseY > 350) && (mouseX < 590) && (mouseY < 430)){
+			slideNum = 5;
+			console.log(slideNum);
+		}
+	}
+}
+
 function showSlide(){
 	if(slideNum === 1){
+		noFill()
+		strokeWeight(4);
+		stroke('#00ff88');
+		rect(190, 150, 400, 80);
+
 		fill(255);
+		noStroke();
 		textSize(50);
 		textFont('Helvetica-Bold');
-		text('Option A',200,200, 200);	
+		text('Option A',200,200, 400);
 
-		text('Option B',200,400, 200);
+		noFill()
+		strokeWeight(4);
+		stroke('#00ff88');
+		rect(190, 350, 400, 80);
+		
+		fill(255);
+		noStroke();
+		textSize(50);
+		textFont('Helvetica-Bold');	
+		text('Option B',200,400, 400);
 	}
 
 	if(slideNum === 2){
