835540847f626085d5f3a3dbd3dde3aedc5f88e7
Lars S. Laichter <lars.laichter@gmail.com>
2019-04-14 22:39:56 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -401,10 +401,10 @@ function showSlide(){
 		noStroke();
 		textSize(40);
 		textFont('Helvetica-Bold');
-		text('For references and acknowledgemnts visit',300,(windowHeight/2)-135, 1000);
+		text('For references and acknowledgemnts visit',300,(windowHeight/2)-35, 1000);
 		textSize(70);
 		textFont('Helvetica-Bold');
-		text('larslaichter.com/keystone',300,(windowHeight/2)-35, 1000);
+		text('larslaichter.com/keystone',300,(windowHeight/2)+65, 1000);
 	}
 }
 
